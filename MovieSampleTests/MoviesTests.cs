10935554c566931dc15d32db746b5ce483f2e816
mikepreston17 <michael.n.preston@gmail.com>
2019-11-21 16:02:12 -0600

    Added Tests

---
+++ MovieSampleTests/MoviesTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MovieSampleTests
+{
+    public class MoviesTests
+    {
+        [Fact]
+        public void CanCreate()
+        {
+            //TODO: Use the sample controller to create new Persons
+        }
+    }
+}
