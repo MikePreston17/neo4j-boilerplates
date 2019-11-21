add8cc8c711fcf49cd982a56f86f00b5daffc65a
mikepreston17 <michael.n.preston@gmail.com>
2019-11-21 16:27:16 -0600

    Added the sample client

---
+++ SampleClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Neo4j.Controllers;
+using Neo4j.Sample.Data;
+using Neo4j.Settings;
+
+namespace SampleClient
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var service = new MovieDataService();
+
+            RunAsync(service).GetAwaiter().GetResult();
+        }
+
+        public static async Task RunAsync(IMovieDataService service)
+        {
+            var settings = ConnectionSettings.CreateBasicAuth("bolt://localhost:7687/db/actors", "neo4j", "test_pwd");
+
+            using (var client = new Neo4JClient(settings))
+            {
+
+                // Create Indices for faster Lookups:
+                await client.CreateIndices();
+
+                // Create Base Data:
+                await client.CreateMovies(service.Movies);
+                await client.CreatePersons(service.Persons);
+                await client.CreateGenres(service.Genres);
+
+                // Create Relationships:
+                await client.CreateRelationships(service.Metadatas);
+            }
+        }
+    }
+}
